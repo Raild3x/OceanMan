c55ee0523303b148252bca4577cb90a03c535980
Raild3x <47638612+Raild3x@users.noreply.github.com>
2019-12-08 19:19:49 -0500

    Test of glfw timegrabber

--- OceanMan/src/Main.cpp
+++ OceanMan/src/Main.cpp
@@ -1,7 +1,8 @@
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <iostream>
 
-int main(void)
-{
+int main(void) {
 	GLFWwindow* window;
 
 	/* Initialize the library */
@@ -9,9 +10,8 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	window = glfwCreateWindow(640, 480, "Take me by the hand.", NULL, NULL);
+	if (!window){
 		glfwTerminate();
 		return -1;
 	}
@@ -20,8 +20,8 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)){
+		std::cout << glfwGetTime() << std::endl;
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
